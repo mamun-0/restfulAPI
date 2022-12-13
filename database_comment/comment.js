4194c41b711d57385dd988074a61abb33851ccae
mamun-0 <md.abdullahalmamun200@gmail.com>
2022-12-13 19:01:03 +0600

    comment database created and add uuid package

---
+++ database_comment/comment.js
@@ -0,0 +1,23 @@
+const { uuid } = require('uuidv4');
+module.exports.comments = [
+  {
+    id: uuid(),
+    username: 'Todd',
+    comment: 'lol that is so funny!',
+  },
+  {
+    id: uuid(),
+    username: 'Skyler',
+    comment: 'I like to go birdwatching with my dog',
+  },
+  {
+    id: uuid(),
+    username: 'Sk8erBoi',
+    comment: 'Plz delete your account, Todd',
+  },
+  {
+    id: uuid(),
+    username: 'onlysayswoof',
+    comment: 'woof woof woof',
+  },
+];
