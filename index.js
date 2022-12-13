f1e41b05c000c809c7d3b5fa0d1d7640b7661421
mamun-0 <md.abdullahalmamun200@gmail.com>
2022-12-13 20:06:04 +0600

    views setup engine

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+const path = reauire('path');
 let comment = require('./database_comment/comment');
+
+//setup the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.listen(3000, () => {
   console.log('PORT LISTENING ON 3000');
